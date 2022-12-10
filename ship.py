1ea22a04a5d3f58fcf31b12009b409ad539b1a9d
Alex Huryn <algurin77@gmail.com>
2022-12-10 18:40:10 +0300

    coding

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initializes the ship and sets its initial position"""
         self.screen = screen
+        self.ai_settings = ai_settings
         # Loading an image of a ship and getting a rectangle.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -13,10 +14,15 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         self.moving_right = False
+        self.moving_left = False
+        self.center = float(self.rect.centerx)
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draws the ship at the current position."""
