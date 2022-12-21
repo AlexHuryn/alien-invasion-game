092d79aace5b601f22f8095e606fd86a58bdcda1
Alex Huryn <algurin77@gmail.com>
2022-12-21 22:28:18 +0300

    coding

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,12 +15,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    bullets = Group()
     bg_color = (230, 230, 230)
     # Running the main game loop
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
